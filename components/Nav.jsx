4737c70d78e7a29f0b35aeafffad884df4ceb576
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-11-05 14:06:00 +0530

    create header and nav

---
+++ components/Nav.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: "home",
+        path: "/",
+    },
+    {
+        name: "services",
+        path: "/services",
+    },
+    {
+        name: "resume",
+        path: "/resume",
+    },
+    {
+        name: "work",
+        path: "/work",
+    },
+    {
+        name: "contact",
+        path: "/contact",
+    }
+
+];
+
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
