ee82902ac9200432420fb1e4d7ee596fba512911
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-11-06 23:40:07 +0530

    update home

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,8 @@
-export default function Home() {
-  return (
-    <main>
-      homepage
-    </main>
-  );
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from  "react-icons/fi";
+
+const Home = () => {
+  return <div>homepage</div>
+};
+
+export default Home;
