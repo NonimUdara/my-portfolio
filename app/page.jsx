32f879ed3d9af5553204adbedebe7f940a7cad8e
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-11-07 12:07:35 +0530

    Add social and images

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+
 const Home = () => {
   return (
     <section className="h-full">
@@ -24,11 +27,15 @@ const Home = () => {
                 <span>Download CV</span>
                 <FiDownload className="text-xl ml-2" />
               </Button>
-              <div>socials</div>
+              <div className="mb-8 xl:mb-0">
+                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex
+                justify-center items-center text-accent text-base hover:bg-accent
+                hover:text-primary hover:transition-all duration-500"/>
+              </div>
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div><Photo /></div>
         </div>
       </div>
     </section>
