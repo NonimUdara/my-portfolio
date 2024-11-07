0f757a24ad6da6d7ba5014303dd9371b0a28ebf9
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-11-07 10:16:51 +0530

    update home with adding global css

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { FiDownload } from  "react-icons/fi";
+import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
   return (
@@ -8,10 +8,24 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left">
-            <span>Software Developer</span>
-            <h1 className="h1">
-              Hello I'm <br /><span>Luke Coleman</span>
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /><span className="text-accent">Luke Coleman</span>
             </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I'm a software developer with a passion for creating innovative and user-friendly software solutions.
+            </p>
+            { /* button and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload className="text-xl ml-2" />
+              </Button>
+              <div>socials</div>
+            </div>
           </div>
           {/* photo */}
           <div>photo</div>
