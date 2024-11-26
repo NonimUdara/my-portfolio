530d7f53cfa053c7b696eae248071cefa7c6d6df
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-11-26 15:29:38 +0530

    update work page left side

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -24,8 +24,8 @@ const projects = [
     category: "frontend",
     title: "project 1",
     description:
-    "Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. ",
-    stack: [{name: "Html 5"}, {name: "Css 3"}, {name: "JavaScript"}],
+      "Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. ",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "JavaScript" }],
     image: 'assets/work/thumb1.png',
     live: "",
     github: "",
@@ -35,8 +35,8 @@ const projects = [
     category: "fullstack",
     title: "project 2",
     description:
-    "Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. ",
-    stack: [{name: "Next.js"}, {name: "Tailwind.css"}, {name: "Node.js"}],
+      "Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. ",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }],
     image: 'assets/work/thumb2.png',
     live: "",
     github: "",
@@ -46,8 +46,8 @@ const projects = [
     category: "frontend",
     title: "project 3",
     description:
-    "Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. ",
-    stack: [{name: "Next.js"}, {name: "Tailwind.css"}],
+      "Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. ",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }],
     image: 'assets/work/thumb3.png',
     live: "",
     github: "",
@@ -58,12 +58,29 @@ const Work = () => {
   const [project, setProject] = useState(projects[0]);
   return (
     <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
-        projects
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col 
+          xl:justify-between order-2 xl:order-none">
+            <div>
+              {/* outline nume */}
+              <div className="text-8xl leading-none font-extrabold text-transparent 
+              text-outline">
+                {project.num}
+              </div>
+              {/* project category */}
+              <h2 className="text-[42px] font-bold leading-none text-white 
+              group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">slider</div>
+        </div>
       </div>
     </motion.div>
   )
