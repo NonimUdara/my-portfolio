a1931aa710ed335eb49d8bd911b248c9bac2ce2d
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-12-12 13:25:48 +0530

    updaate home socials

--- app/page.jsx
+++ app/page.jsx
@@ -48,8 +48,8 @@ const Home = () => {
               </Button>
               <div className="mb-8 xl:mb-0">
                 <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex
-                justify-center items-center text-accent text-base hover:bg-accent
-                hover:text-primary hover:transition-all duration-500" />
+                justify-center items-center text-base bg-primary hover:bg-white
+                hover:border-primary hover:transition-all duration-500" />
               </div>
             </div>
           </div>
--- components/Social.jsx
+++ components/Social.jsx
@@ -1,20 +1,23 @@
 import Link from "next/link";
-
 import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const socials = [
-    { icon: <FaGithub />, path: "https://github.com/NonimUdara" },
-    { icon: <FaLinkedinIn />, path: "" },
-    { icon: <FaYoutube />, path: "" },
-    { icon: <FaTwitter />, path: "" },
-]
+    { icon: <FaGithub />, path: "https://github.com/NonimUdara", textColor: "text-white hover:text-black" },
+    { icon: <FaLinkedinIn />, path: "https://linkedin.com", textColor: "text-blue-700" },
+    { icon: <FaYoutube />, path: "https://youtube.com", textColor: "text-red-600" },
+    { icon: <FaTwitter />, path: "https://twitter.com", textColor: "text-blue-400" },
+];
 
 const Social = ({ containerStyles, iconStyles }) => {
     return (
         <div className={containerStyles}>
             {socials.map((item, index) => {
                 return (
-                    <Link href={item.path} key={index} className={iconStyles}>
+                    <Link
+                        href={item.path}
+                        key={index}
+                        className={`${iconStyles} ${item.textColor}`}
+                    >
                         {item.icon}
                     </Link>
                 );
@@ -23,4 +26,4 @@ const Social = ({ containerStyles, iconStyles }) => {
     );
 };
 
-export default Social
+export default Social;
