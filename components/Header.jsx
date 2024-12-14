1bb433a1a4723451e7b070da5a67304622e44b57
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-12-14 22:05:22 +0530

    add spinning logo to header with adding some to tailwind config css

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,17 +7,30 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
     return (
-
         <header className="py-8 xl:py-12 text-white">
             <div className="container mx-auto flex justify-between items-center">
                 {/* Logo */}
                 <Link href="/">
-                    <h1 className="text-4xl font-semibold">
-                        Nonim <span className="text-accent">.</span>
-                    </h1>
+                    <div class="flex items-center justify-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            className="w-16 h-16 text-accent animate-spin-slow"
+                        >
+                            <path
+                                d="M12 2l3 7h7l-5 4 2 7-6-5-6 5 2-7-5-4h7l3-7z"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            />
+                        </svg>
+                    </div>
+
+
+
                 </Link>
-
-
                 {/* desktop nav & hire me button */}
                 <div className="hidden xl:flex items-center gap-8">
                     <Nav />
@@ -30,11 +43,9 @@ const Header = () => {
                 <div className="xl:hidden">
                     <MobileNav />
                 </div>
-
             </div>
         </header>
-
     );
 };
 
-export default Header
+export default Header;
