d91ca03475ac4ba271373067c60fa55211698674
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2025-02-23 01:53:16 +0530

    update project 2

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -34,11 +34,11 @@ const projects = [
   },
   {
     num: "02",
-    category: "Vehicle Automotive Spare Parts Sales Platform",
-    title: "project 1",
+    category: "Web Application School Management System",
+    title: "project 2",
     description:
-      "This system fosters strong client relationships by offering a user-friendl platform for Sri Lankan citizens to buy and sell genuine spare parts effortlessly. With efficient categorization, a seamless buying experience, and the ability to locate nearby repair centers, it serves as a one-stop solution for automotive needs, catering to both Administrators and Users.",
-    stack: [{ name: "ReactJS" }, { name: "React Native" }, { name: "ExpressJS" }, { name: "NodeJS" }, { name: "MongoDB" }, { name: "GitHub" }, { name: "Microsoft Azure" }],
+      "E-Learning management system for school A/L students with including all functionalities for teachers students and admins.",
+    stack: [{ name: "AngularJS" }, { name: "ExpressJS" }, { name: "NodeJS" }, { name: "MongoDB" }, { name: "GitHub" }],
     image: '/assets/work/thumb1.jpg',
     live: "",
     github: "",
